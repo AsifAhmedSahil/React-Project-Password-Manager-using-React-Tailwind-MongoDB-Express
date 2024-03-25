ce67a3302a081b008b63f3ec031a08ab5eb0e53c
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-03-25 16:21:43 +0600

    form setup

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -5,14 +5,21 @@ import { FaEyeSlash } from "react-icons/fa";
 const Manager = () => {
   const ref = useRef()
   const [status,setStatus] = useState(true)
+  const [form,setForm] = useState({site:'',username:'',password:''})
 
   const showPassword = () => {
 
-    
-
-    
   }
-  
+
+  const savePassword = () =>{
+    console.log(form)
+
+  }
+
+  const handleChange = (e) =>{
+    setForm({...form,[e.target.name]: e.target.value})
+
+  }
   return (
     <>
       <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"></div>
@@ -28,27 +35,33 @@ const Manager = () => {
         </p>
         <div className="flex flex-col p-4 gap-6 text-black items-center">
           <input
+          value={form.site}
+          onChange={handleChange}
           placeholder="Enter Wensite Url"
             className="rounded-full w-full border border-green-500 px-4 py-1"
             type="text"
-            name=""
+            name="site"
             id=""
           />
           <div className="flex gap-6 w-full justify-between ">
             <input
+            value={form.username}
+            onChange={handleChange}
             placeholder="Enter Username"
               className="rounded-full w-full border border-green-500 px-4 py-1"
               type="text"
-              name=""
+              name="username"
               id=""
             />
             <div className="relative">
 
             <input
+            value={form.password}
+            onChange={handleChange}
             placeholder="Enter Password"
               className="rounded-full w-full border border-green-500 px-4 py-1"
               type="text"
-              name=""
+              name="password"
               id=""
             />
             <span  onClick={() => setStatus(!status)} className="absolute right-[6px] top-[8px] cursor-pointer">{status? <FaEye width={20}/> : <FaEyeSlash/>}</span>
@@ -56,7 +69,7 @@ const Manager = () => {
             </div>
             
           </div>
-          <button className="flex justify-center items-center gap-3 bg-green-400 w-fit px-5 py-2 rounded-full">
+          <button onClick={savePassword} className="flex justify-center items-center gap-3 bg-green-400 w-fit px-5 py-2 rounded-full">
           <lord-icon
             src="https://cdn.lordicon.com/slmechys.json"
             trigger="hover"
