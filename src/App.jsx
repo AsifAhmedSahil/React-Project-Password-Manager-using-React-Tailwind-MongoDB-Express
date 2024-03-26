18462a93e2841ccbe8ea13e934d8d991250c17ff
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-03-27 02:30:29 +0600

    delete applied

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
+// import { useState } from 'react'
+
 
 import './App.css'
 import Navbar from './components/Navbar'
 import Manager from './components/Manager'
 
 import Last from './components/Last'
+// import Manager2 from './components/Manager2'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
@@ -16,6 +18,7 @@ function App() {
       <Navbar/>
       <div className='min-h-[74vh]'>
       <Manager></Manager>
+      {/* <Manager2></Manager2> */}
       </div>
       <Last></Last>
     </>
